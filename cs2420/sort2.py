9367df02e246ed5d5d018fa4001c72e5b64ee777
Jason Reynolds <jasonthewhite97@gmail.com>
2016-09-21 08:24:42 -0600

    Added sort2, vim configurations

---
+++ cs2420/sort2.py
@@ -0,0 +1,7 @@
+
+def generate_list(length):
+	list1 = []
+	for i in range(length):
+		list1.append(random.randint(0, length))
+	return list1
+
---
+++ vim/.vimrc
@@ -0,0 +1,7 @@
+set tabstop=4
+set number
+set wildmenu
+set hlsearch
+filetype indent on
+syntax on
+colorscheme molokai 
