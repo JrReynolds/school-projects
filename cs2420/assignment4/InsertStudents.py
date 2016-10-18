67dec20ec82c0c2a3c68489d29dc06880d88f053
Jason Reynolds <shrackx@gmail.com>
2016-10-18 10:38:01 -0600

    Completed Student class, test code in main, insertstudent function

--- cs2420/assignment4/InsertStudents.py
+++ cs2420/assignment4/InsertStudents.py
@@ -1,4 +1,5 @@
 import time
+import Student
 
 
 def timeSomething(func, *args):
@@ -7,4 +8,22 @@ def timeSomething(func, *args):
     end_time = time.time() - start_time
     return end_time
 
+def InsertStudent(A, student):
+    if student.getSSN() in A:
+        print "ERROR: Student {} {} (SSN {}) already in database".format(student.getFName(), student.getLName(), student.getSSN())
+    else:
+        A.append(student)
 
+def main():
+    a = Student.Student("Henry", "Jones", "123-45-7899", "indianajones@awesome.com", "40")
+    b = Student.Student("Anakin", "Skywalker", "357-89-7642", "darthvader@darkside.com", "30")
+    c = Student.Student("Anakin", "Skywalker", "357-89-7642", "darthvader@darkside.com", "30")
+    print a == b
+    print b == c
+
+    database = []
+    InsertStudent(database, a)
+    InsertStudent(database, b)
+    InsertStudent(database, c)
+
+main()
